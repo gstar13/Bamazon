73c54a66033870d017acb06475bd735de4a8e01f
GStar13 <ginazavala004@gmail.com>
2019-03-11 00:57:17 -0700

    works

--- bamazon.js
+++ bamazon.js
@@ -1,28 +1,28 @@
-var mysql= require("mysql");
+var mysql = require("mysql");
 var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: "localhost",
     //my port
-    port:3306,
+    port: 3306,
     //username
     user: "root",
     //your password
-    password:'Jim!2018',
+    password: 'Jim!2018',
     database: "bamazon"
 
 });
 
-connection.connect(function(err){
-    if(err) throw err;
+connection.connect(function (err) {
+    if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-     readProducts();
+    readProducts();
 });
 //prompt
 inquirer.prompt([
     {
-type:"input",
-name:"productId",
-message: "Please enter the ID of the product you would like to buy?",
+        type: "input",
+        name: "productId",
+        message: "Please enter the ID of the product you would like to buy?",
     },
     {
         type: "input",
@@ -31,38 +31,73 @@ message: "Please enter the ID of the product you would like to buy?",
     },
 
 ])
-.then(function(answer){
-    var itemToBuy = answer.productId;
-   
-    var qty = parseInt(answer.productUnits);
-   
-    var query = "SELECT * FROM products WHERE ?";
-    connection.query(query, {item_id:itemToBuy}, function(err,res){
-        if(err) throw err;
-        if(res.length === 0 ){
-            console.log("invalid ID, Please enter a valid Item ID number.");
-            readProducts();
-        } else {
-            var itemData = res[0];
-           
-        console.log("You have selected to purchase " + qty);
-        console.log(itemData.product_name);
-        console.log("from the " + itemData.department_name + " department.");
+    .then(function (answer) {
+        var itemToBuy = answer.productId;
 
-        }   
+        var qty = parseInt(answer.productUnits);
+
+        var query = "SELECT * FROM products WHERE ?";
+        connection.query(query, { item_id: itemToBuy }, function (err, res) {
+            if (err) throw err;
+            if (res.length === 0) {
+                console.log("invalid ID, Please enter a valid Item ID number.");
+                readProducts();
+            } else {
+                var itemData = res[0];
+
+                console.log("You have selected to purchase " + qty);
+                console.log(itemData.product_name);
+                console.log("from the " + itemData.department_name + " department.");
+                if (itemData.stock_quantity < qty) {
+                    console.log("There are not enough in stock for that transaction");
+                    exitStrategy();
+                }
+                else {
+                    var amtRemaining = itemData.stock_quantity - qty;
+                    console.log("There are now "+ amtRemaining+ " "+ itemData.product_name + " remaining." );
+                }
+            }
+        })
+
+
+        //     //     var updateQuery = "UPDATE products WHERE ?  ";
+        //     //     connection.query(updateQuery,{itemData.stock_quantity : qty}, function (err, res) {
+        //     //         if (err) throw err;;
+        //     //         console.log( "The new item quantity is " + )
+        //     //     })
+
+        //     // }
+
+        // }
+
+
+
     })
+function exitStrategy() {
 
-    // if(itemToBuyID.stock_quantity>+qty){
-    //     console.log("there is not enough for this") ; 
-    // }
-//   connection.end();
-})
+    connection.end();
+}
+
 function readProducts() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res){
-        if(err) throw err;
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("Available Products At G's BAMAZON STORE");
+        //  console.log("Item ID------Product Name-------Available Quantity---------Price");
+
+
         //Log all results of the Select statement
-        console.log(res);
-       
+        for (var i = 0; i < res.length; i++) {
+            var productData = res[i];
+
+            console.table("Item ID: " + productData.item_id);
+            console.table("Product Name: " + productData.product_name);
+            console.table("Available Quantity: " + productData.stock_quantity);
+            console.table("Price: " + productData.price);
+            console.log("-----------------------------------------------------------------------");
+        }
     });
 }
+
+
+
